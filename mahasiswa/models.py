388742a3d40284de82caad74a5e838e315754490
Agus Eka Pramudita <agusekapramudita@gmail.com>
2025-12-11 12:00:30 +0700

    Update halaman mahasiswa dan perbaikan models

--- mahasiswa/models.py
+++ mahasiswa/models.py
@@ -4,7 +4,7 @@ class Mahasiswa(models.Model):
     nim = models.CharField(max_length=15, unique=True)
     nama = models.CharField(max_length=100)
     programstudi = models.CharField(max_length=50)
-    angkatan = models.CharField()
+    angkatan = models.CharField(max_length=10)
 
     def __str__(self):
         return f"{self.nim} - {self.nama}"
