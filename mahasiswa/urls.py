8ca4d8a2a93dca3ac3b743f81d776183fdff88a1
Agus Eka Pramudita <agusekapramudita@gmail.com>
2025-12-12 01:15:32 +0700

    first deploy

--- mahasiswa/urls.py
+++ mahasiswa/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
      path('tambah/', views.tambah_mahasiswa, name='tambah_mahasiswa'),
      path('edit/<int:id>/', views.edit_mahasiswa, name='edit_mahasiswa'),
      path('hapus/<int:id>/', views.hapus_mahasiswa, name='hapus_mahasiswa'), 
-         
+     path('', RedirectView.as_view(url='daftar/')),  
 ]
